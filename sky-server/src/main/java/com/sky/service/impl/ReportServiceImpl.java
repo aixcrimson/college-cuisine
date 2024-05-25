301a1ef6777f8f8cfcce3e3d0bdd93c9818a0d0a
GotohHitori <hitori150221@outlook.com>
2024-05-25 16:47:38 +0800

    用户统计功能

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,9 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
-import io.swagger.annotations.ApiOperation;
+import com.sky.vo.UserReportVO;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,8 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 统计指定时间区间内的营业额数据
@@ -58,11 +61,58 @@ public class ReportServiceImpl implements ReportService {
             turnoverList.add(turnover);
         }
 
-        // 创建TurnoverReportVO对象封装数据返回
+        // 3.创建TurnoverReportVO对象封装数据返回
         TurnoverReportVO turnoverReportVO = TurnoverReportVO.builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
                 .build();
         return turnoverReportVO;
+    }
+
+    /**
+     * 统计指定时间区间内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        // 1.创建集合用于存放从begin到end范围内每天的日期
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        // 2.创建集合用于存放每天新增的用户数量
+        List<Integer> newUserList = new ArrayList<>();
+        // select count(id) from user where create_time > ? and create_time < ?
+        // 3.创建集合用于存放每天总用户数量
+        List<Integer> totalUserList = new ArrayList<>();
+        // select count(id) from user where create_time < ?
+
+        // 4.封装条件查询，将查询结果存放到集合中
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+            Map map = new HashMap();
+
+            map.put("end", endTime); // 先添加end参数 统计总用户
+            Integer totalUser = userMapper.countByMap(map);
+
+            map.put("begin", beginTime); // 再添加begin参数，统计每日新增的用户
+            Integer newUser = userMapper.countByMap(map);
+
+            totalUserList.add(totalUser);
+            newUserList.add(newUser);
+        }
+
+        UserReportVO userReportVO = UserReportVO.builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
+                .build();
+        return userReportVO;
     }
 }
